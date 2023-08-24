d4a63d3a44b1833d96ddeffb609f3e9c63d4ae6a
Jake Harris <jakec.harris@gmail.com>
2023-08-24 17:13:03 -0500

    Added mRNA translation method

--- SequenceUser.py
+++ SequenceUser.py
@@ -10,7 +10,7 @@ class SequenceUser:
     def __init__(self, text):
         self.text = text
     
-    # Convert downloaded FASTA .txt file into Python dict 
+    # convert downloaded FASTA .txt file into Python dict 
     def fasta_dictionary(self):
         fasta_dict = {}
         header = None
@@ -27,3 +27,36 @@ class SequenceUser:
             # meant for final gene since there is no last ">" marker
             fasta_dict[header] = ''.join(seq_list)
         return fasta_dict
+    
+    # translate RNA string into protein
+    def mrna_translation(self):
+        with open(self.text, 'r') as f:
+            seq_list = []
+            for line in f:
+                seq_list.append(line.strip())
+            mrna = ''.join(seq_list)
+        aa_table = {'UUU': 'F', 'CUU': 'L', 'AUU': 'I', 'GUU': 'V',
+                    'UUC': 'F', 'CUC': 'L', 'AUC': 'I', 'GUC': 'V',
+                    'UUA': 'L', 'CUA': 'L', 'AUA': 'I', 'GUA': 'V',
+                    'UUG': 'L', 'CUG': 'L', 'AUG': 'M', 'GUG': 'V',
+                    'UCU': 'S', 'CCU': 'P', 'ACU': 'T', 'GCU': 'A',
+                    'UCC': 'S', 'CCC': 'P', 'ACC': 'T', 'GCC': 'A',
+                    'UCA': 'S', 'CCA': 'P', 'ACA': 'T', 'GCA': 'A',
+                    'UCG': 'S', 'CCG': 'P', 'ACG': 'T', 'GCG': 'A',
+                    'UAU': 'Y', 'CAU': 'H', 'AAU': 'N', 'GAU': 'D',
+                    'UAC': 'Y', 'CAC': 'H', 'AAC': 'N', 'GAC': 'D',
+                    'UAA': '_', 'CAA': 'Q', 'AAA': 'K', 'GAA': 'E',
+                    'UAG': '_', 'CAG': 'Q', 'AAG': 'K', 'GAG': 'E',
+                    'UGU': 'C', 'CGU': 'R', 'AGU': 'S', 'GGU': 'G',
+                    'UGC': 'C', 'CGC': 'R', 'AGC': 'S', 'GGC': 'G',
+                    'UGA': '_', 'CGA': 'R', 'AGA': 'R', 'GGA': 'G',
+                    'UGG': 'W', 'CGG': 'R', 'AGG': 'R', 'GGG': 'G'}
+        protein = ''
+        start = mrna.find('AUG')
+        for i in range(start, len(mrna), 3):
+            codon = mrna[i:3+i]
+            if codon == 'UAG' or codon == 'UAA' or codon == 'UGA':
+                break
+            else:
+                protein += aa_table[codon]
+        return protein
